c6604965da9bf5a50bcdd484c22f87ae2f324200
Golliard <golliard.info@gmail.com>
2024-11-11 16:49:21 +0100

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    assetPrefix: './',
+    basePath: '/mon-blog4',
+    images: {
+      unoptimized: true,
+    },
+  };
+  export default nextConfig;
+  
